46fb961bd331f969b9e1388594dc008aa179031a
Arif Rahman S <Arif_R673@mitrais.com>
2019-02-04 14:41:32 +0700

    add test post n delete, fix seed

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
-var data = {
-  id: 10
-};
+var password = '123abc!'
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+ bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password,salt, (err,hash) => {
+    console.log(hash);
+    
+  })
+})
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$jO.rHQjO8ac3h1nu0sipR.UHOPX6AGGjeKfd/RQGs0hmS.MwHwlI2'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+
+
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
